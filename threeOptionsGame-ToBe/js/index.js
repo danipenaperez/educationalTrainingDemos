94ba42a8244ee72d0cacbe076b680dcf135b3b00
Daniel Peña Perez <danipenaperez@gmail.com>
2023-10-27 09:42:55 +0200

    tobe

--- threeOptionsGame-ToBe/js/index.js
+++ threeOptionsGame-ToBe/js/index.js
@@ -8,7 +8,7 @@ var options =[
     {word:"It "+placeholder+" broken", correct: "is" , example: "Eso esta roto" },
     {word:"She "+placeholder+" listening to music", correct: "is" , example: "Ella esta escuchando musica"},
     {word:"He "+placeholder+" reading a book", correct: "is", example: "El esta leyendo un libro"},
-    {word:"We "+placeholder+" going to museum TV", correct: "are", example: "Nosotros estamos yendo al museo"},
+    {word:"We "+placeholder+" going to the museum.", correct: "are", example: "Nosotros estamos yendo al museo"},
     {word:"You "+placeholder+" riding a horse", correct: "are", example: "Vosotros estais montando a caballo" },
     {word:"They "+placeholder+" playing chess", correct: "are", example: "Ellos estan jugando al ajedrez"},
 
@@ -18,11 +18,11 @@ var options =[
     {word:"She "+placeholder+" playing chess", correct: "is" , example: "Ella esta jugando al ajedrez"},
     {word:"Daniel "+placeholder+" reading a book", correct: "is", example: "Daniel esta leyendo un libro"},
     {word:"Martin and me "+placeholder+" riding a horse", correct: "are", example: "Martin y yo estamos montado a caballo"},
-    {word:"You "+placeholder+" going to museum TV", correct: "are", example: "Vosotros estais yendo al museo" },
+    {word:"You "+placeholder+" going to the museum ", correct: "are", example: "Vosotros estais yendo al museo" },
     {word:"Maria and Luis "+placeholder+" playing chess", correct: "are", example: "Maria y luis estan jugando al ajedrez"},
 
     {word:"I "+placeholder+" playing chess.", correct: "am", example: "Yo estoy jugando al ajedrez."},
-    {word:"I "+placeholder+" going to museum TV.", correct: "am", example: "Yo estoy yendo al museo."},
+    {word:"I "+placeholder+" going to the museum .", correct: "am", example: "Yo estoy yendo al museo."},
     {word:"I "+placeholder+" riding a horse.", correct: "am", example: "Yo estoy montado a caballo."}
 ];
 
