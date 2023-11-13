c36e0e193cc3155b1f2083abd35e6dd0ddb317e0
Daniel Peña Perez <danipenaperez@gmail.com>
2023-11-13 17:39:14 +0100

    changed to type nimber inputext

--- templates/templates.js
+++ templates/templates.js
@@ -24,7 +24,7 @@ templates.getInputTextHTMLTemplate = function (id){
     
         var content =  '<div class="form-group mb-3">'+
                 '<label for="'+id+'" class="form-label">Tu Respuesta:</label>'+
-                '<input type="text" class="form-control" id="'+id+'"></input>'+
+                '<input type="number" class="form-control" id="'+id+'"></input>'+
                 // '</div>'+
                 // '<div class="mb-3">'+
                 '<button id="check_'+id+'_Btn" type="button" class="btn btn-default btn-primary" style="padding-top: 5;margin-top: 5px;" >Comprobar</button>'+
