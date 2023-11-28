2aeb48082dd4d22737a56b2a4bc010d4e37a42c1
Daniel Peña Perez <danipenaperez@gmail.com>
2023-11-28 18:44:59 +0100

    nuevas opcones

--- data/marta/english/pastsimple.js
+++ data/marta/english/pastsimple.js
@@ -12,7 +12,8 @@ var main_options = [
     {word:"¿Que significa el verbo find?", correct: "encontrar", explanation: "<strong>Find</strong> significa <br><strong>Encontrar</strong>"},
     {word:"¿Que significa el verbo live?", correct: "vivir", explanation: "<strong>Live</strong> significa <br><strong>vivir</strong>" },
     {word:"¿Que significa el verbo sail?", correct: "navegar", explanation: "<strong>Sail</strong> significa <br><strong>Navegar</strong>" },
-    {word:"¿Que significa el verbo start?", correct: "empezar", explanation: "<strong>Start</strong> significa <br><strong>Empezar</strong>" }
+    {word:"¿Que significa el verbo start?", correct: "empezar", explanation: "<strong>Start</strong> significa <br><strong>Empezar</strong>" },
+    {word:"Find significa encontrar, pero ¿como se escribe find en pasado?", correct: "found", explanation: "<strong>Find</strong> significa encontrar y se escribe en pasado <br><strong>Found</strong>" }
 
 ];
 
