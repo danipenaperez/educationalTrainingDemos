43c4c35931cef81d3c4e97100bd481d4aea24567
Daniel Peña Perez <danipenaperez@gmail.com>
2023-12-16 21:05:23 +0100

    Added preguntas

--- dani_generic_working/js/data/gameData.js
+++ dani_generic_working/js/data/gameData.js
@@ -22,7 +22,14 @@ var options = [
     {ask:"Si tu eres yo y yo soy tu '<b>¿Quien es mas tonto de los dos?</b>'", correctAnswer: "yo", example: "Yo estoy montado a caballo."},
     {ask:"'<b>¿cual es la  contraseña de seguridad?</b>'", correctAnswer: "triangulo", example: "Yo estoy montado a caballo."},
     {ask:"'<b>¿Uno mas uno?</b>'", correctAnswer: "tres", example: "Yo estoy montado a caballo."},
-    {ask:"'<b>¿Quien ataca por la noche?</b>'", correctAnswer: "detective cacas", example: "Yo estoy montado a caballo."}
+    {ask:"'<b>¿Quien ataca por la noche?</b>'", correctAnswer: "detective cacas", example: "Yo estoy montado a caballo."},
+    {ask:"'<b>Te quiero , te adoro eres mi ...</b>'", correctAnswer: "tesoro", example: "Yo estoy montado a caballo."},
+    {sentence:"¿Cual pie huele peor ? [selectOption| ,mama,papa,martin,marta,tomas]", correctAnswer: "¿Cual pie huele peor ? tomas", example: "Yo estoy montado a caballo."},
+    {ask:"¿Que come un oso polar?", correctAnswer: "albondigas", example: "Yo estoy montado a caballo."},
+    {sentence:"¿Cual es el hipo mas peligroso ? [selectOption| ,el de la cacho,el del pedote salvaje,el de la maldad,el de la cacota y sus amigos]", correctAnswer: "¿Cual es el hipo mas peligroso ? el de la maldad", example: "Yo estoy montado a caballo."},
+    {ask:"'<b>ya lo ...</b>''", correctAnswer: "sepo", example: "Yo estoy montado a caballo."},
+    {ask:"'quierelo, huelelo , es el ...'", correctAnswer: "pie de tomas", example: "Yo estoy montado a caballo."},
+    {ask:"Chocolate y ...", correctAnswer: "copita", example: "Yo estoy montado a caballo."}
     // , {chooseOne:"Que significa '<b>Hello</b>' [chooseOne|hola,dos,tres,cuatro,cinco,seis,siete,ocho,nueve]", correctAnswer: "Hola", example: "Yo estoy montado a caballo."}
     
     //{image:"https://cdn11.bigcommerce.com/s-88x43f5nfi/images/stencil/1280x1280/products/149/412/kc-nau-6-model-_1__2__70435.1564668558.jpg?c=1", correct: "compass Rose", example: "Rosa de los vientos"}
